e492ba23d52558ec333ddec5a4007b20e7eac326
logesh028 <logesht2003@gmail.com>
2024-02-07 09:44:18 +0530

    Designed Login Page

--- frontend/src/components/LoginPage/index.tsx
+++ frontend/src/components/LoginPage/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, TextField } from "@mui/material";
+import { Button, Paper, Stack, TextField } from "@mui/material";
 import { useFormik } from "formik";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -44,45 +44,62 @@ const HomePage = () => {
     });
 
     return (
-        <Stack sx={{ alignItems: 'center', marginTop: '10%' }}>
-            <img
-                src={Logo}
-                alt=""
-                style={{
-                    width: '100%',  // Set the initial width to 100%
-                    maxWidth: '200px',  // Set the maximum width (adjust as needed)
-                    height: 'auto',  // Maintain aspect ratio
-                }}
-            />
-            <br />
-            {accountNotFound && <span style={{ color: 'red' }}><br />Account Not Found</span>}
-            <form>
-                <br />
-                <TextField
-                    id="outlined-basic"
-                    label="Email"
-                    variant="outlined"
-                    name="email"
-                    sx={{ width: '40ch' }}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
+        <Stack
+            sx={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <Paper elevation={6} sx={{
+                padding: 2,
+                borderRadius: 2,
+                textAlign: 'center',
+                maxWidth: '400px',
+                width: '90%',
+            }}>
+                <img
+                    src={Logo}
+                    alt=""
+                    style={{
+                        width: '100%',  
+                        maxWidth: '200px',  
+                        height: 'auto',
+                        margin: 'auto',
+                    }}
                 />
-                {formik.touched.email && formik.errors.email && <span style={{ color: 'red' }}><br />{formik.errors.email}</span>}
-                <br /><br />
-                <TextField
-                    id="outlined-basic"
-                    label="Password"
-                    variant="outlined"
-                    name="password"
-                    sx={{ width: '40ch' }}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.password && formik.errors.password && <span style={{ color: 'red' }}><br />{formik.errors.password}</span>}
                 <br />
-                <br />
-                <Button variant="contained" sx={{ width: '45ch' }} disabled={!(formik.isValid && formik.dirty)} onClick={userSignIn}>Sign In</Button><br /><br />
-            </form>
+                {accountNotFound && <span style={{ color: 'red' }}><br />Account Not Found</span>}
+                <form>
+                    <br />
+                    <TextField
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        name="email"
+                        sx={{ width: '100%' }}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email && <span style={{ color: 'red' }}><br />{formik.errors.email}</span>}
+                    <br /><br />
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                        name="password"
+                        sx={{ width: '100%' }}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.password && formik.errors.password && <span style={{ color: 'red' }}><br />{formik.errors.password}</span>}
+                    <br />
+                    <br />
+                    <Button variant="contained" sx={{ width: '100%' }} disabled={!(formik.isValid && formik.dirty)} onClick={userSignIn}>Sign In</Button><br /><br />
+                </form>
+            </Paper>
         </Stack>
     );
 }

d930b7766c7e083af8203facfe5934da21b1b66e
Mathan M <mathan44862@gmail.com>
2024-02-21 10:58:34 +0530

    chnages

--- frontend/src/components/HomePage/Header/Navbar/index.tsx
+++ frontend/src/components/HomePage/Header/Navbar/index.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
+import SchoolIcon from '@mui/icons-material/School';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import SchoolIcon from '@mui/icons-material/School';
-import { useNavigate } from 'react-router-dom';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 import { jwtDecode } from 'jwt-decode';
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 interface UserDetails {
@@ -48,6 +48,9 @@ function ResponsiveAppBar() {
     if(page.toLowerCase()==="calendar"){
         navigate('/');
     }
+    else if(page.toLowerCase()==="user"){
+      navigate('/');
+    } 
     else{
         navigate('/' + page.toLowerCase());
     }
